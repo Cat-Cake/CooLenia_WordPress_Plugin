ab32fff075bd1c24d12e2ea55bb7af683c3ff4eb
lucas <lucas@pop-os.localdomain>
2023-03-27 14:47:11 +0200

    Connecting tarte au citron to CooLenia

--- coolenia.php
+++ coolenia.php
@@ -1,19 +1,65 @@
 <?php
 /**
  * Plugin Name: Coolenia
- * Description: Plugin de test
+ * Description: Plugin pour les cookies
  * Author: NoName
  */
 
-function first_function_hello_world() {
-    echo "<p>Hello world!</p>";
+define( 'WP_DEBUG', true );
+function starting_tarte_au_citron() {
+    wp_enqueue_script( 'mytarteaucitron', './tarteaucitron.js-1.10.0/tarteaucitron.js' );
 }
 
-add_action('admin_notices', 'first_function_hello_world');
+add_action('wp_enqueue_scripts', 'starting_tarte_au_citron');
 
-function first_plugin_hello_title($title) {
-    $custom_title = 'Hello World ' . $title;
-    return $custom_title;
+function tarte_au_citron() {
+    ?>
+    <script src="/wp-content/plugins/CooLenia/tarteaucitron.js-1.10.0/tarteaucitron.js"></script>
+    <script type="text/javascript">
+        tarteaucitron.init({
+            "privacyUrl": "", /* Privacy policy url */
+            "bodyPosition": "bottom", /* or top to bring it as first element for accessibility */
+
+            "hashtag": "#tarteaucitron", /* Open the panel with this hashtag */
+            "cookieName": "tarteaucitron", /* Cookie name */
+
+            "orientation": "middle", /* Banner position (top - bottom) */
+
+            "groupServices": false, /* Group services by category */
+            "serviceDefaultState": "wait", /* Default state (true - wait - false) */
+
+            "showAlertSmall": false, /* Show the small banner on bottom right */
+            "cookieslist": false, /* Show the cookie list */
+
+            "closePopup": false, /* Show a close X on the banner */
+
+            "showIcon": true, /* Show cookie icon to manage cookies */
+            //"iconSrc": "", /* Optionnal: URL or base64 encoded image */
+            "iconPosition": "BottomRight", /* BottomRight, BottomLeft, TopRight and TopLeft */
+
+            "adblocker": false, /* Show a Warning if an adblocker is detected */
+
+            "DenyAllCta" : true, /* Show the deny all button */
+            "AcceptAllCta" : true, /* Show the accept all button when highPrivacy on */
+            "highPrivacy": true, /* HIGHLY RECOMMANDED Disable auto consent */
+
+            "handleBrowserDNTRequest": false, /* If Do Not Track == 1, disallow all */
+
+            "removeCredit": false, /* Remove credit link */
+            "moreInfoLink": true, /* Show more info link */
+
+            "useExternalCss": false, /* If false, the tarteaucitron.css file will be loaded */
+            "useExternalJs": false, /* If false, the tarteaucitron.js file will be loaded */
+
+            //"cookieDomain": ".my-multisite-domaine.fr", /* Shared cookie for multisite */
+
+            "readmoreLink": "", /* Change the default readmore link */
+
+            "mandatory": true, /* Show a message about mandatory cookies */
+            "mandatoryCta": true /* Show the disabled accept button when mandatory on */
+        });
+    </script>
+    <?php
 }
 
-add_filter('the_title', 'first_plugin_hello_title');
+add_action('wp_head', 'tarte_au_citron');
